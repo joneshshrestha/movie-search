269e4b2df1ca84e27be890da0df9834c9bcd86e7
joneshshrestha <contact@joneshshrestha.com>
2020-06-14 18:08:16 +0545

    update

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
+import SearchMovies from "./searchMovies";
 
 class Main extends React.Component {
   render() {
     return (
       <div className="container">
-        <h1 title="">Hello World updated!</h1>;
+        <h1 className="title">Movie Search</h1>
+        <SearchMovies />
       </div>
     );
   }
